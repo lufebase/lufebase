b2355ee1eebce9ea55b30b30bb72c7870aa5282b
lufebase <lufebase@gmail.com>
2016-11-11 15:06:35 -0600

    HolaMundo JAva

---
+++ HolaMundo/src/inicio/Principal.java
@@ -0,0 +1,14 @@
+
+package inicio;
+
+/**
+ *
+ * @author Donlad Arias Ramos
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!");
+    }
+ 
+}
